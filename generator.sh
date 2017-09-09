a1ed379a1a3b89364045eb32f726f303d69f9d25
Eric Lehner <ericj@buffalo.edu>
2017-09-08 21:07:16 -0400

    finish generator

--- generator.sh
+++ generator.sh
@@ -6,9 +6,26 @@ test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:$PATH
 test -r ~/.bash_profile && echo 'export PATH="$(brew --prefix)/bin:$PATH"' >>~/.bash_profile
 echo 'export PATH="$(brew --prefix)/bin:$PATH"' >>~/.profile
 
-# install tmux and neovim
+### neovim
 brew install neovim
-brew install tmux
+# install neovim dotfiles
+mkdir -p .config/nvim
+ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
+ln -s ~/dotfiles/ftplugin ~/.config/nvim/ftplugin
+# install Plug
+mkdir .config/nvim/autoload
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# install plugins
+nvim +PlugInstall +qall
 
-# install dotfiles
-# Maybe when I make sure it doesn't need to be private >.>
+### tmux
+brew install tmux
+# install tpm
+mkdir -p .tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# install tmux dotfiles
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+tmux source ~/.tmux.conf
+# install tpm modules
+~/.tmux/plugins/tpm/bin/install_plugins
