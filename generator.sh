984ed5692bfe21a6d42f80fc30926a32bc0e55b6
Eric Lehner <ericj@buffalo.edu>
2017-09-08 21:24:05 -0400

    add analytics shut-off

--- generator.sh
+++ generator.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # install linux brew
-sudo apt-get install build-essential curl file git python-setuptools ruby
+apt-get install build-essential curl file git python-setuptools ruby
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
 test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$PATH"
 test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 test -r ~/.bash_profile && echo 'export PATH="$(brew --prefix)/bin:$PATH"' >>~/.bash_profile
 echo 'export PATH="$(brew --prefix)/bin:$PATH"' >>~/.profile
+
+# turn off analytics
+brew analytics off
 
 ### neovim
 brew install neovim
